d312cf408aaa58f08ab4034a58c1fe26f32be32e
“tiagoluis12” <tiago7164@hotmail.com>
2023-09-13 15:11:45 +0100

    Implemented, Create Order, Get All Orders, Get Single Order, Get Current User Orders and Update Order

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -18,7 +18,7 @@ router
   .post(authenticateUser, createOrder)
   .get(authenticateUser, authorizePermissions("admin"), getAllOrders);
 
-router.route("/ShowAllMyOrders").get(authenticateUser, getCurrentUserOrders);
+router.route("/showAllMyOrders").get(authenticateUser, getCurrentUserOrders);
 
 router
   .route("/:id")
